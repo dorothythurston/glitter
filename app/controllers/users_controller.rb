b64a9fb9df1666b2bc98fcc302ebf66069285cc4
Dorothy Thurston <dorothy.thur@gmail.com>
2015-10-18 19:30:07 -0400

    User's Items can be viewed on User page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @items = @user.items
+  end
+
   private
 
   def user_params
--- spec/support/features/item_helpers.rb
+++ spec/support/features/item_helpers.rb
@@ -5,6 +5,10 @@ module Features
       click_button I18n.t('items.new.submit')
     end
 
+    def display_item(item_text)
+      have_content(item_text)
+    end
+
     def example_text
       "here is some text"
     end
