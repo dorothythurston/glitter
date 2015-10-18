604b05ae9d4cdb26f9715c697ca37dbf717c7b18
Dorothy Thurston <dorothy.thur@gmail.com>
2015-10-18 19:15:55 -0400

    Speed up tests with Monban Backdoor

--- spec/features/user_creates_item_spec.rb
+++ spec/features/user_creates_item_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 feature "User creates item" do
   scenario "successfully" do
     user = create(:user)
-    sign_in(user)
     visit root_path(as: user)
 
     create_item(item_text)
--- spec/features/user_deletes_own_item_spec.rb
+++ spec/features/user_deletes_own_item_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 feature "User deletes item" do
   scenario "successfully" do
     user = create(:user)
-    sign_in(user)
     visit root_path(as: user)
 
     create_item(example_text)
