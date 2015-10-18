6e5dab56938cdbb81cce2fbec0cf7526ee21e953
Dorothy Thurston <dorothy.thur@gmail.com>
2015-10-18 16:02:14 -0400

    Add locale value for Welcome message

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -4,6 +4,6 @@ feature "User visits homepage" do
   scenario "successfully" do
     visit root_path
 
-    expect(page).to have_css 'h1', text: 'Welcome'
+    expect(page).to have_content I18n.t('dashboards.show.welcome_message')
   end
 end
