c173769a9c6cc629ae6d8f5577c23b05a9e86067
Dorothy Thurston <dorothy.thur@gmail.com>
2015-11-01 18:11:38 -0500

    Add dependent destroy to posts

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   has_many :glitterings, as: :glitterable, dependent: :destroy
+  has_many :activities, as: :subject, dependent: :destroy
 
   def self.paginated(page, per)
     page(page).per(per)
